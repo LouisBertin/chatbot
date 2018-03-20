4d7d68f439eea946a1308207dd0d73cd011c1da1
LouisBertin <bertin.louis7@gmail.com>
2018-03-20 15:27:41 +0100

    clean code

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/hello', (req, res) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ a: 1 }, null, 3));
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, function () {
+    console.log(`Listening on ${ PORT }`)
+})
