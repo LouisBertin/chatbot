eb08af4b9ad6da4c3a0af38d2250ece39c4e16aa
LouisBertin <bertin.louis7@gmail.com>
2018-03-20 17:01:39 +0100

    action route

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get('/json', function (req, res) {
     res.json({"foo": "bar"});
 })
 
+app.post('/action', function (req, res) {
+    let test = req.body.result.parameters.metro-line-number
+
+    res.json({
+        "speech": "this text is spoken out loud if the platform supports voice interactions",
+        "displayText": test,
+    });
+})
+
 app.listen(PORT, function () {
     console.log(`Listening on ${ PORT }`)
 })
