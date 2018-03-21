feb7a86ae1378e57a03776b7540aaa2b7cdd4296
LouisBertin <bertin.louis7@gmail.com>
2018-03-21 16:47:11 +0100

    card marker

--- index.js
+++ index.js
@@ -267,9 +267,6 @@ app.post('/action', function (req, res) {
 
             break;
         case "webhook.user.data.workplace.yes":
-            let workplace = req.body.result.parameters['street-address']
-            let fbUserId = req.body.originalRequest.data.sender.id
-
             console.log(req.body)
 
             break;
