b679655217cb6749290eed8df6ecafccf29e3743
Yoann <contact@yoannvie.fr>
2018-03-21 13:02:11 +0100

    add debug

--- index.js
+++ index.js
@@ -114,7 +114,11 @@ app.post('/action', function (req, res) {
             break;
         case "webhook.travel.time.from":
             var from = req.body.result.parameters['street-address-from'];
+            console.log('from');
+            console.log(from);
             var to = req.body.result.parameters['street-address-to'];
+            console.log('to');
+            console.log(to);
 
             googleMapsClient.geocode({
                 address: from
