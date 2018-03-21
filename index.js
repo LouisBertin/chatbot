21c3c69e446bf1e70fe8c55239f3d81eebf85fe9
Yoann <contact@yoannvie.fr>
2018-03-22 00:59:36 +0100

    #route : add log

--- index.js
+++ index.js
@@ -280,6 +280,7 @@ app.post('/action', function (req, res) {
                                 if (!err) {
                                     var message = '';
                                     var steps = response.json.routes[0].legs[0].steps;
+                                    console.log(response.json.routes[0]);
                                     for (var i = 0, len = steps.length; i < len; i++) {
 
                                         if (steps[i].travel_mode == 'TRANSIT') {
