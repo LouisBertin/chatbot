d8e3bc92a77afcace3fbd83a7d31316cbb3dbac1
LouisBertin <bertin.louis7@gmail.com>
2018-03-22 11:51:48 +0100

    card marker

--- helper.js
+++ helper.js
@@ -1,3 +1,7 @@
+const connectionString = "postgres://rmwavbfvtbeyss:3825e353072eeb06ef4687fc7655ae68b7a99c4c7c51e26e67026801cd30ceee@ec2-54-204-44-140.compute-1.amazonaws.com:5432/dalifn8rdhdpfm"
+var client = new pg.Client(connectionString);
+client.connect();
+
 function getUserAddress (id, addressCode) {
     client.query('SELECT * FROM users WHERE id = '+id+', address_code = ' + addressCode, function(err, result) {
         return result.rows
