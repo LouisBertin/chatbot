3296b22ec7145072e4d9d0716f24a234171f7013
LouisBertin <bertin.louis7@gmail.com>
2018-03-22 11:56:36 +0100

    card marker

--- helper.js
+++ helper.js
@@ -1,3 +1,4 @@
+const pg = require('pg');
 const connectionString = "postgres://rmwavbfvtbeyss:3825e353072eeb06ef4687fc7655ae68b7a99c4c7c51e26e67026801cd30ceee@ec2-54-204-44-140.compute-1.amazonaws.com:5432/dalifn8rdhdpfm"
 var client = new pg.Client(connectionString);
 client.connect();
