e7fea73ce2b4040c244ba50e8526ad6de1a88843
LouisBertin <bertin.louis7@gmail.com>
2018-03-22 14:54:40 +0100

    card marker

--- index.js
+++ index.js
@@ -499,8 +499,8 @@ app.post('/action', function (req, res) {
                     var lat = response.json.results[0].geometry.location.lat
                     var lng = response.json.results[0].geometry.location.lng
 
-                    client.query('INSERT INTO users(id, address_code, address_txt, lat, lng) values($1, $2, $3, $4, $5)',
-                        [parseInt(fbuserIdHome), 'home', formated_adress, lat, lng]);
+                    client.query('INSERT INTO users(fb_id, address_code, address_txt, lat, lng) values($1, $2, $3, $4, $5)',
+                        [fbuserIdHome, 'home', formated_adress, lat, lng]);
                 }
             });
 
